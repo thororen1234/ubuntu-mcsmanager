b566a352aeb613faa55444c6e86a13132cec99ca
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-11-03 15:08:54 -0500

    no

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,9 @@ while ! docker info >/dev/null 2>&1; do
   sleep 1
 done
 
-export MCSMANAGER_DEPENDENCIES_PATH=/opt/mcsmanager/daemon/lib
-
-/opt/node-v20.12.2-linux-x64/bin/node /opt/mcsmanager/daemon/app.js &
-/opt/node-v20.12.2-linux-x64/bin/node /opt/mcsmanager/web/app.js
+cd /opt/mcsmanager/daemon &&
+/opt/node-v20.12.2-linux-x64/bin/node app.js &
+cd ../web &&
+/opt/node-v20.12.2-linux-x64/bin/node app.js
 
 wait
