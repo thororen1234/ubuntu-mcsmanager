7028aa274e6e59f976d4759308f975a29ebed076
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-11-03 15:17:09 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,10 @@ while ! docker info >/dev/null 2>&1; do
   sleep 1
 done
 
-cd /opt/mcsmanager/daemon &&
+cd /opt/mcsmanager/daemon
 /opt/node-v20.12.2-linux-x64/bin/node app.js &
-cd ../web &&
+
+cd /opt/mcsmanager/web
 /opt/node-v20.12.2-linux-x64/bin/node app.js
 
 wait
